c0845279a375c4e0cd5d9a2be74dbfb7e12e828e
Pepijn Overbeeke <pepijn.overbeeke@gmail.com>
2021-12-01 11:06:31 +0100

    Day 1

--- app/Day1.hs
+++ app/Day1.hs
@@ -3,14 +3,19 @@ module Main where
 import Lib
 
 solve1 :: [String] -> Int
-solve1 xs = head $ [x * y | x <- ys, y <- ys, x + y == 2020]
+solve1 xs = length $ filter (>0) $ zipWith (-) (tail ys) ys
   where
     ys = map read xs
 
+sumTriples :: [Int] -> [Int]
+sumTriples [a, b, c] = [a + b + c]
+sumTriples (x:y:z:xs) = (x + y + z):(sumTriples (y:z:xs))
+
 solve2 :: [String] -> Int
-solve2 xs = head $ [x * y * z | x <- ys, y <- ys, z <- ys, x + y + z == 2020]
+solve2 xs = length $ filter (>0) $ zipWith (-) (tail zs) zs
   where
     ys = map read xs
+    zs = sumTriples ys
 
 main :: IO ()
 main = mainWrapper "day1" solve1 solve2
